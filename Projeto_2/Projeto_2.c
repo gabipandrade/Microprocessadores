06dace451dab3f1e94686fa8304d6e2e4fea66d2
João Pedro Gomes <joaop.gomes@usp.br>
2024-11-01 17:48:54 -0300

    Adiciona mudança de frequência de incremento com base em interrupções externas

--- Projeto_2/Projeto_2.c
+++ Projeto_2/Projeto_2.c
@@ -1,5 +1,5 @@
-unsigned char TMR0H_preset = 0xB;
-unsigned char TMR0L_preset = 0xDC;
+unsigned char TMR0H_preset = 0xFF;
+unsigned char TMR0L_preset = 0x00;
 
 unsigned char displayNum = 0;
 
@@ -18,15 +18,15 @@ void out7Seg(unsigned char n)
   switch (n)
   {      // acionamento do display de 7 segmentos do kit EasyPIC (PORTD)
     case 0:{ latd = 0b00111111; break;}   // 0 no display de 7seg.
-    case 1:{ latd = 0b00000110; break;}   // 1 no display de 7seg.
+    case 1:{ latd = 0b10000110; break;}   // 1 no display de 7seg.
     case 2:{ latd = 0b01011011; break;}   // 2 no display de 7seg.
-    case 3:{ latd = 0b01001111; break;}   // 3 no display de 7seg.
+    case 3:{ latd = 0b11001111; break;}   // 3 no display de 7seg.
     case 4:{ latd = 0b01100110; break;}   // 4 no display de 7seg.
-    case 5:{ latd = 0b01101101; break;}   // 5 no display de 7seg.
+    case 5:{ latd = 0b11101101; break;}   // 5 no display de 7seg.
     case 6:{ latd = 0b01111101; break;}   // 6 no display de 7seg.
-    case 7:{ latd = 0b00000111; break;}   // 7 no display de 7seg.
+    case 7:{ latd = 0b10000111; break;}   // 7 no display de 7seg.
     case 8:{ latd = 0b01111111; break;}   // 8 no display de 7seg.
-    case 9:{ latd = 0b01101111; break;}   // 9 no display de 7seg.
+    case 9:{ latd = 0b11101111; break;}   // 9 no display de 7seg.
     default:{ latd = 0b00000000; break;} // entrada inválida, apagar display
   }
 
@@ -50,18 +50,34 @@ void INTERRUPCAO_HIGH() iv 0x0008 ics ICS_AUTO {
     TMR0L += TMR0L_preset;
     INTCON.TMR0IF = 0;   //Não esquecer de zerar a Flag
   }
+
+  else if(INTCON.INT0IF == 1){
+    T0CON.TMR0ON = 1;// Inicia timer, ligando o display
+    
+    TMR0H_preset = 0x0B;
+    TMR0L_preset = 0xDC;
+
+    INTCON.INT0IF = 0;
+  }
+  else if(INTCON3.INT1IF == 1){
+    T0CON.TMR0ON = 1;// Inicia timer, ligando o display
+    
+    TMR0H_preset = 0xC2;
+    TMR0L_preset = 0xF7;
+
+    INTCON3.INT1IF = 0;
+  }
 }
 
 void configInterrupt(){
   //Configuração Global das interrupções GIE   - chave geral que sinaliza que o
   //programa usará interrupções
-  INTCON.GIE = 1; // Alta prioridade   - usar somente SE RCON.IPEN =1;
-
-  RCON.IPEN = 1; //habilita níveis de prioridade - permite que uma interrupção
-  //tenha prioridade no tratamento caso ocorra ao mesmo tempo que outro tipo de
-  // interrupção ou que interrompa uma interrupção de baixa priordade em atendimento
+  INTCON.GIE = 1;
 
   INTCON.TMR0IE = 1;   //Habilita a interrupção individual do TIMER0
+  INTCON.INT0IE = 1; //Habilita a interrupção específica INT0
+  INTCON3.INT1IP = 1; // Interrupcão INT1 em alta prioridade
+  INTCON3.INT1IE = 1; //Habilita a interrupção específica INT1
 }
 
 void configMCU()
@@ -71,11 +87,13 @@ void configMCU()
   // Config. das portas
   TRISD = 0;    // PORTD como saída  (usar LED)
   PORTD = 0;    // LED inicialmente OFF
+  
+  TRISB = 0xFF; // PORTB como entradas
 }
 
 void configTimer(){
   // Timer0 Registers:// 16-Bit Mode; Prescaler=1:32; TMRH Preset=B; TMRL Preset=DC; Freq=1,00Hz; Period=1,00 s
-  T0CON.TMR0ON = 1;// Timer0 On/Off Control bit:1=Enables Timer0 / 0=Stops Timer0
+  T0CON.TMR0ON = 0;// Timer0 On/Off Control bit:1=Enables Timer0 / 0=Stops Timer0
   T0CON.T08BIT = 0;// Timer0 8-bit/16-bit Control bit: 1=8-bit timer/counter / 0=16-bit timer/counter
   T0CON.T0CS   = 0;// TMR0 Clock Source Select bit: 0=Internal Clock (CLKO) / 1=Transition on T0CKI pin
   T0CON.T0SE   = 0;// TMR0 Source Edge Select bit: 0=low/high / 1=high/low
@@ -93,6 +111,6 @@ void main() {
   configTIMER();
   configInterrupt();
   
-  
+  // Pooling
   while(1);
 }
--- Projeto_2/Projeto_2.cp
+++ Projeto_2/Projeto_2.cp
@@ -1,6 +1,6 @@
 #line 1 "C:/Users/eugom/OneDrive/Documentos/MICROCONTROLADORES/SEL0614/Projeto_2/Projeto_2.c"
-unsigned char TMR0H_preset = 0xB;
-unsigned char TMR0L_preset = 0xDC;
+unsigned char TMR0H_preset = 0xFF;
+unsigned char TMR0L_preset = 0x00;
 
 unsigned char displayNum = 0;
 
@@ -19,16 +19,16 @@ void out7Seg(unsigned char n)
  switch (n)
  {
  case 0:{ latd = 0b00111111; break;}
- case 1:{ latd = 0b00000110; break;}
+ case 1:{ latd = 0b10000110; break;}
  case 2:{ latd = 0b01011011; break;}
- case 3:{ latd = 0b01001111; break;}
+ case 3:{ latd = 0b11001111; break;}
  case 4:{ latd = 0b01100110; break;}
- case 5:{ latd = 0b01101101; break;}
+ case 5:{ latd = 0b11101101; break;}
  case 6:{ latd = 0b01111101; break;}
- case 7:{ latd = 0b00000111; break;}
+ case 7:{ latd = 0b10000111; break;}
  case 8:{ latd = 0b01111111; break;}
- case 9:{ latd = 0b01101111; break;}
- default:{ latd = 0b00000000; ; break;}
+ case 9:{ latd = 0b11101111; break;}
+ default:{ latd = 0b00000000; break;}
  }
 
 }
@@ -44,12 +44,29 @@ void INTERRUPCAO_HIGH() iv 0x0008 ics ICS_AUTO {
 
  if(INTCON.TMR0IF == 1)
  {
- incrementUntil9(&displayNum);
  out7Seg(displayNum);
+ incrementUntil9(&displayNum);
 
  TMR0H += TMR0H_preset;
  TMR0L += TMR0L_preset;
  INTCON.TMR0IF = 0;
+ }
+
+ else if(INTCON.INT0IF == 1){
+ T0CON.TMR0ON = 1;
+
+ TMR0H_preset = 0x0B;
+ TMR0L_preset = 0xDC;
+
+ INTCON.INT0IF = 0;
+ }
+ else if(INTCON3.INT1IF == 1){
+ T0CON.TMR0ON = 1;
+
+ TMR0H_preset = 0xC2;
+ TMR0L_preset = 0xF7;
+
+ INTCON3.INT1IF = 0;
  }
 }
 
@@ -58,11 +75,10 @@ void configInterrupt(){
 
  INTCON.GIE = 1;
 
- RCON.IPEN = 1;
-
-
-
  INTCON.TMR0IE = 1;
+ INTCON.INT0IE = 1;
+ INTCON3.INT1IP = 1;
+ INTCON3.INT1IE = 1;
 }
 
 void configMCU()
@@ -72,11 +88,13 @@ void configMCU()
 
  TRISD = 0;
  PORTD = 0;
+
+ TRISB = 0xFF;
 }
 
 void configTimer(){
 
- T0CON.TMR0ON = 1;
+ T0CON.TMR0ON = 0;
  T0CON.T08BIT = 0;
  T0CON.T0CS = 0;
  T0CON.T0SE = 0;
