c6c364deeae6ff64928716b0af3330f6d6616253
João Pedro Gomes <joaop.gomes@usp.br>
2024-11-01 14:57:37 -0300

    Adiciona arquivos iniciais do projeto 2

---
+++ Projeto_2/Projeto_2.asm
@@ -0,0 +1,8 @@
+
+_main:
+
+;Projeto_2.c,1 :: 		void main() {
+;Projeto_2.c,3 :: 		}
+L_end_main:
+	GOTO        $+0
+; end of _main
---
+++ Projeto_2/Projeto_2.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/eugom/OneDrive/Documentos/MICROCONTROLADORES/SEL0614/Projeto_2/Projeto_2.c"
+void main() {
+
+}
